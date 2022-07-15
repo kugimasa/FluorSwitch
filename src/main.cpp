d27fc097f75b81e79af6d58583ae1ed3321d42a7
kugimasa <kugimania@gmail.com>
2022-07-15 23:44:39 +0900

    Undo switching loop order.

--- src/main.cpp
+++ src/main.cpp
@@ -158,8 +158,8 @@ void render(unsigned char *data, unsigned int nx, unsigned int ny, int ns) {
   // 時間計測開始
   start = std::chrono::system_clock::now();
 
-  for (int i = 0; i < nx; i++) {
-    for (int j = 0; j < ny; j++) {
+  for (int j = 0; j < ny; j++) {
+    for (int i = 0; i < nx; i++) {
       vec3 col(0, 0, 0);
       for (int s = 0; s < ns; s++) {
         double u = double(i + drand48()) / double(nx);
