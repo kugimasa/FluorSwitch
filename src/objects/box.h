2f3d8c9c38abc0c354b5f622b1192e00254767be
kugimasa <kugimania@gmail.com>
2022-08-28 23:14:27 +0900

    Remove flip_face from box faces.

--- src/objects/box.h
+++ src/objects/box.h
@@ -43,14 +43,14 @@ box<mat>::box(const point3 &p0, const point3 &p1, shared_ptr<mat> ptr) {
   box_min = p0;
   box_max = p1;
 
-  sides.add(make_shared<flip_face<mat>>(make_shared<xy_rect<mat>>(p0.x(), p1.x(), p0.y(), p1.y(), p1.z(), ptr)));
-  sides.add(make_shared<flip_face<mat>>(make_shared<xy_rect<mat>>(p0.x(), p1.x(), p0.y(), p1.y(), p0.z(), ptr)));
+  sides.add(make_shared<xy_rect<mat>>(p0.x(), p1.x(), p0.y(), p1.y(), p1.z(), ptr));
+  sides.add(make_shared<xy_rect<mat>>(p0.x(), p1.x(), p0.y(), p1.y(), p0.z(), ptr));
 
   sides.add(make_shared<xz_rect<mat>>(p0.x(), p1.x(), p0.z(), p1.z(), p1.y(), ptr));
   sides.add(make_shared<xz_rect<mat>>(p0.x(), p1.x(), p0.z(), p1.z(), p0.y(), ptr));
 
-  sides.add(make_shared<flip_face<mat>>(make_shared<yz_rect<mat>>(p0.y(), p1.y(), p0.z(), p1.z(), p1.x(), ptr)));
-  sides.add(make_shared<flip_face<mat>>(make_shared<yz_rect<mat>>(p0.y(), p1.y(), p0.z(), p1.z(), p0.x(), ptr)));
+  sides.add(make_shared<yz_rect<mat>>(p0.y(), p1.y(), p0.z(), p1.z(), p1.x(), ptr));
+  sides.add(make_shared<yz_rect<mat>>(p0.y(), p1.y(), p0.z(), p1.z(), p0.x(), ptr));
 }
 
 template<typename mat>
